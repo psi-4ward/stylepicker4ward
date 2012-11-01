d9dd9abc7335fcb7134987ca8a63431cd4b29344
Christoph Wiechert <wio@psitrax.de>
2012-11-02 00:45:18 +0100

    Fixed: edit multiple not supported → button removed

--- dca/tl_stylepicker4ward.php
+++ dca/tl_stylepicker4ward.php
@@ -38,13 +38,7 @@ $GLOBALS['TL_DCA']['tl_stylepicker4ward'] = array
 		),
 		'global_operations' => array
 		(
-			'all' => array
-			(
-				'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
-				'href'                => 'act=select',
-				'class'               => 'header_edit_all',
-				'attributes'          => 'onclick="Backend.getScrollOffset();" accesskey="e"'
-			)		
+			
 		),
 		'operations' => array
 		(
