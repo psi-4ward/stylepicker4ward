310a7990a9816a238bd8a3d740e896ee8e3ba841
Christoph Wiechert <wio@psitrax.de>
2013-02-05 17:05:04 +0100

    Added: show the css-classes bottom of the edit-icons for content elements

---
+++ dca/tl_content.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @copyright 4ward.media 2013 <http://www.4wardmedia.de>
+ * @author Christoph Wiechert <wio@psitrax.de>
+ */
+ 
+$GLOBALS['TL_DCA']['tl_content']['list']['sorting']['child_record_callback'] = array('tl_content_stylepicker', 'addClassNames');
+
+
+class tl_content_stylepicker extends tl_content
+{
+
+	public function addClassNames($arrRow)
+	{
+		$ret = '';
+		$cssID = deserialize($arrRow['cssID']);
+		$ret .= $this->addCteType($arrRow);
+		if($cssID[1])
+		{
+			$ret .= '<div style="position: absolute; top: 30px; color: #999; right: 10px">'.$GLOBALS['TL_LANG']['tl_content']['_cssclasses'].''.$cssID[1].'</div>';
+		}
+		return $ret;
+	}
+}
---
+++ languages/de/tl_content.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @copyright 4ward.media 2013 <http://www.4wardmedia.de>
+ * @author Christoph Wiechert <wio@psitrax.de>
+ */
+
+$GLOBALS['TL_LANG']['tl_content']['_cssclasses'] = 'CSS-Klassen: ';
